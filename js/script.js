139af6efa19c32813883b37f43bd01fdc70e19b0
Carlo Costa <carlo.costa92@gmail.com>
2025-03-10 15:27:55 +0100

    feat: display elements in page

--- js/script.js
+++ js/script.js
@@ -38,3 +38,46 @@ const teamMembers = [
 ];
 
 // console.log(teamMembers);
+
+
+// ciclo for per prendermi tutte le proprietà e pusharle nel mio html
+
+const container= document.querySelector("div.container");
+
+for(let i=0;i<teamMembers.length; i++){
+  const teamMembersElements=document.createElement('div');
+  teamMembersElements.classList.add('card-container');
+
+  teamMembersElements.innerHTML = `            <div class="image">
+                <img src="${teamMembers[i].img}" alt="${teamMembers[i].name}">
+            </div>
+             <div class="info">
+                <h4>${teamMembers[i].name}</h4>
+                <p>${teamMembers[i].role}</p>
+                <div class="email">
+                    <p>${teamMembers[i].email}</p>
+               </div>
+            </div>
+         </div> `
+
+  container.appendChild(teamMembersElements);
+
+}
+
+// test per legare struttura delle card 
+
+
+// const div = document.querySelector("div.container");
+// div.innerHTML += `<div class="card-container">
+//             <div class="image">
+//                 <img src="img/female1.png" alt="female1">
+//             </div>
+//             <div class="info">
+//                 <h4>aa</h4>
+//                 <p>a</p>
+//                 <div class="email">
+//                     <p>aaa</p>
+//                 </div>
+//             </div>
+//         </div> `
+
